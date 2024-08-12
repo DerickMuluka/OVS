b1abddda152f34738c091975efd88d71041579b0
Derick Muluka <mulukaderick@gmail.com>
2024-08-12 16:20:47 +0300

    Add array of election positions with corresponding IDs

---
+++ includes/positions.php
@@ -0,0 +1,10 @@
+<?php
+$positions = [
+    'President' => 1,
+    'Secretary General' => 2,
+    'Finance Secretary' => 3,
+    'Health and Environmental Secretary' => 4,
+    'Academic Secretary' => 4,
+    'Sports and Entertainment Secretary' => 5
+];
+?>
